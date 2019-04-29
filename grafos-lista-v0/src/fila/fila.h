7a4f2c5de420b7e00076b2f4409ea23ed19c19ff
unknown <ednei.freiberger@OFC_7039.portobellonet.local>
2019-04-29 11:02:18 -0300

    20190419@1102

---
+++ grafos-lista-v0/src/fila/fila.h
@@ -0,0 +1,22 @@
+/*
+ * fila.h
+ *
+ *  Created on: 29 de abr de 2019
+ *      Author: ednei.freiberger
+ */
+
+#ifndef FILA_H_INCLUDED
+#define FILA_H_INCLUDED
+
+
+typedef struct filas fila_t;
+
+fila_t * cria_fila (void);
+
+void enqueue(void *dado, fila_t *fila);
+void* dequeue(fila_t *fila);
+
+int fila_vazia(fila_t *fila);
+void libera_fila(fila_t* fila);
+
+#endif // FILA_H_INCLUDED
