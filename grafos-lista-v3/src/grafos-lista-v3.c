f519b5f1aacdfce11e18a62f8d83765fb75b6c41
arianeam <47660205+arianeam@users.noreply.github.com>
2019-06-18 11:21:35 -0300

    teste

--- grafos-lista-v3/src/grafos-lista-v3.c
+++ grafos-lista-v3/src/grafos-lista-v3.c
@@ -3,7 +3,7 @@
  Name        : grafos-lista-v0.c
  Author      : ednei
  Version     :
- Copyright   : Your copyright notice
+ Copyright   : Your copyright notice: juca
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
