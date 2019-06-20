ae51565e1d0d77de0ff38a54695ff675892a361c
arianeam <47660205+arianeam@users.noreply.github.com>
2019-06-20 15:51:34 -0300

    pacman 20062019

--- pacman/sprites.asm
+++ pacman/sprites.asm
@@ -1,9 +1,5 @@
 .include "graphics.inc"
-.include "bitmap.inc"   #setpixel
 .include "init.inc"
-
-
-
 
 .text 0x00410000
 .globl main
@@ -21,7 +17,7 @@ main:
     main2:
     move $a0,$t8
     move $a1,$t9
-    li   $a2,14
+    li   $a2,8
     jal  draw_sprite
     addi $t8, $t8, 1
 	
@@ -45,22 +41,58 @@ draw_grid:
 #$a0-> x   $a1-> y   $a2->sprite_id [0.. n sprite]
 .globl draw_sprite
 draw_sprite:
-    la $t3, $zero       # ponteiro i
-    la $t4, $zero       # ponteiro j
-    la $t0, colors      # cor[0]
-    la $t1, sprites     # sprite[0]
-    mul $t1, $a2, 49    # calcula 0ff_set sprite
-    add $t3, $t3, $t1   # x[i]
 
-        
-    lb $a2, 0($t3)
-    mul $a2, $a2, 4
-    jal set_pixel
-    add $t3, $t3, 1
-    add $t4, $t4, 1
+#pilha
+#|  a0  | 0(sp)
+#|  a1  | 4(sp)
+#|  a2  | 8(sp)
+#|  s0  | 12(sp)
+#|  s1  | 16(sp)
+#|  s2  | 20(sp)
+#|  ra  | 24(sp)
+#|empty | 28(sp)
+
+addi $sp, $sp, -32
+sw $ra, 24($sp)
+
+   
+    la $s0, colors      # cor[0]
+    la $s1, sprites     # sprite[0]
+    mul $t2, $a2, 49    # calcula 0ff_set sprite, sprite_id*49
+    add $s1, $s1, $t2   # x[i]
+    addi $t3, $a0, 7 #X+7
+    addi $t4, $a1, 7 #Y+7
+    li $s2, 0
+
+draw_sprite_for_i:
+
+bge $a0, $t3, draw_sprite_for_i_end
+add $s2, $s2, $a0
+draw_sprite_for_j:
+bge $a1, $t4, draw_sprite_for_j_end
+add $s2, $s2, $a1
+add $s1, $s1, $s2
+
+   
+    lb $a2, 0($s1) #carrega byte do sprite
+    mul $a2, $a2, 4 #converte byte pra int 
+    add $a2, $a2, $s0
+    lw $a2, 0($a2)
+    sw $a0, 0($sp)
+    sw $a1, 4($sp)
     
+    jal set_pixel  
+   
+    addi $a1, $a1, 1
+  
+j draw_sprite_for_j
+    addi $a0, $a0, 1
+draw_sprite_for_j_end:
+j draw_sprite_for_i
 
-
+draw_sprite_for_i_end:
+    lw $ra, 24($sp)
+    addi $sp, $sp, 32
     jr   $ra
 
 # set_pixel(int X, int Y, int color)  
@@ -73,4 +105,6 @@ set_pixel:
    sll $a0, $a0, 2
    add $a0, $a0, $t0
    sw  $a2, 0($a0)
+   lw $a0, 0($sp)
+   lw $a1, 4($sp)
    jr  $ra
