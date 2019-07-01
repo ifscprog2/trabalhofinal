452587501d28e14069fd1a4f18cd7fa6e634ebde
arianeam <47660205+arianeam@users.noreply.github.com>
2019-07-01 11:32:43 -0300

    importa_dados

---
+++ grafos-lista-v3/src/importa_dados.h
@@ -0,0 +1,7 @@
+#ifndef IMPORTA_DADOS_H_INCLUDED
+#define IMPORTA_DADOS_H_INCLUDED
+#include "grafo/grafo.h"
+
+grafo_t* le_vertices_arquivo(const char* filename);
+
+#endif // IMPORTA_DADOS_H_INCLUDED
